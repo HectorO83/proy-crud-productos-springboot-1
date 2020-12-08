d128d6c0da7290e87ee55639c023e4cc2b3b95dd
HectorO83 <hectororbegoso83@gmail.com>
2020-12-08 20:33:51 -0300

    Se sube arquitectura base del proyecto

---
+++ src/test/java/com/hector/springboot/app/ProyCrudProductosSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hector.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyCrudProductosSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
